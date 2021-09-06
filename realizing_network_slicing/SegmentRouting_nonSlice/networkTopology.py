3770adf147f394685962cd47d78ca7f88c560964
Suyankai <63870120+Suyankai@users.noreply.github.com>
2021-09-06 11:33:23 +0200

    Add files via upload

---
+++ realizing_network_slicing/SegmentRouting_nonSlice/networkTopology.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import OVSKernelSwitch, RemoteController
+from mininet.cli import CLI
+from mininet.link import TCLink
+
+
+class NetworkSlicingTopo(Topo):
+    def __init__(self):
+        # Initialize topology
+        Topo.__init__(self)
+
+        # Create template host, switch, and link
+        host_config = dict(inNamespace=True)   # 这里可能是问题所在
+        link_config = dict()
+        host_link_config = dict()
+
+        # Create switch nodes
+        for i in range(2):
+            sconfig = {"dpid": "%016x" % (i + 1)}
+            self.addSwitch("s%d" % (i + 1), **sconfig)
+
+        # Create host nodes
+        # for i in range(4):
+        #     self.addHost("h%d" % (i + 1), **host_config)
+
+        # creating the host one by one with addresses
+        self.addHost("h1", ip="10:0:0:0:0:0:0:01")
+        self.addHost("h2", ip="10:0:0:0:0:0:0:02")
+        self.addHost("h3", ip="10:0:0:0:0:0:0:03")
+        self.addHost("h4", ip="10:0:0:0:0:0:0:04")
+
+
+        # Add switch links
+        self.addLink("s1", "s2", **link_config)
+
+        # Add host links
+        self.addLink("h1", "s1", **host_link_config)
+        self.addLink("h2", "s1", **host_link_config)
+        self.addLink("h3", "s2", **host_link_config)
+        self.addLink("h4", "s2", **host_link_config)
+
+
+topos = {"networkslicingtopo": (lambda: NetworkSlicingTopo())}
+
+if __name__ == "__main__":
+    topo = NetworkSlicingTopo()
+    net = Mininet(
+        topo=topo,
+        switch=OVSKernelSwitch,
+        build=False,
+        autoSetMacs=True,
+        autoStaticArp=True,
+        link=TCLink,
+    )
+    controller = RemoteController("c1", ip="127.0.0.1", port=6633)
+    net.addController(controller)
+    net.build()
+    net.start()
+    CLI(net)
+    net.stop()
+
---
+++ realizing_network_slicing/SegmentRouting_nonSlice/nonSlicing.py
@@ -0,0 +1,97 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ipv6
+from ryu.lib.packet import in_proto
+from ryu.lib.packet import packet_base
+from ryu.lib.packet import udp
+from ryu.lib.packet import tcp
+from ryu.lib.packet import icmp
+
+
+class TrafficSlicing(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(TrafficSlicing, self).__init__(*args, **kwargs)
+
+
+        self.ipv6_to_port = {
+            # outport = self.ipv6_to_port[dpid][mac_src_add]
+            1: {"10:0:0:0:0:0:0:01": 2, "10:0:0:0:0:0:0:02": 3},
+            2: {"10:0:0:0:0:0:0:03": 2, "10:0:0:0:0:0:0:04": 3},
+        }
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install the table-miss flow entry. no match found in the flow table
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    # Add flow entry
+    def add_flow(self, datapath, priority, match, actions):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # construct flow_mod message and send it.
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                match=match, instructions=inst)
+        # The instance of the OFPFlowMod
+        # class is generated and the message is sent to the OpenFlow switch using the Datapath.send_msg() method.
+        datapath.send_msg(mod)
+
+    def _send_package(self, msg, datapath, in_port, actions):
+        data = None
+        ofproto = datapath.ofproto
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = datapath.ofproto_parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=msg.buffer_id,
+            in_port=in_port,
+            actions=actions,
+            data=data,
+        )
+        datapath.send_msg(out)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # how to build the match
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        in_port = msg.match["in_port"]
+
+        pkt = packet.Packet(msg.data)  # This might be very useful for SRv6
+        ip = pkt.get_protocol(ipv6.ipv6)
+
+        dst = ip.dst  # destination
+        src = ip.src  # source
+
+        dpid = datapath.id
+        if dst in self.ipv6_to_port[dpid]:
+            out_port = self.ipv6_to_port[dpid][dst]
+            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+            match = datapath.ofproto_parser.OFPMatch(ipv6_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+            self._send_package(msg, datapath, in_port, actions)
+
+
+        else:
+            out_port = 1
+            actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
+            match = datapath.ofproto_parser.OFPMatch(ipv6_dst=dst)
+            self.add_flow(datapath, 1, match, actions)
+            self._send_package(msg, datapath, in_port, actions)
